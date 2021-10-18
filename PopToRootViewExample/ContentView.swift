7611c1105428a3ad528b9be5bb1b83dadf4a2d93
Russell Gordon <rgordon@lcs.on.ca>
2021-10-18 17:53:10 -0400

    Example of how to pop back to a root view in SwiftUI

--- PopToRootViewExample/ContentView.swift
+++ PopToRootViewExample/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isActive : Bool = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink(destination: ContentView2(rootIsActive: $isActive),
+                           isActive: $isActive) {
+                Text("Hello, World!")
+            }
+            .isDetailLink(false)
+            .navigationBarTitle("Root")
+        }
+    }
+}
+
+struct ContentView2: View {
+    @Binding var rootIsActive : Bool
+
+    var body: some View {
+        NavigationLink(destination: ContentView3(rootIsActive: $rootIsActive)) {
+            Text("Hello, World #2!")
+        }
+        .isDetailLink(false)
+        .navigationBarTitle("Two")
+        .onAppear {
+            print("root is active?: \(rootIsActive)")
+        }
+    }
+}
+
+struct ContentView3: View {
+    @Binding var rootIsActive : Bool
+
+    var body: some View {
+        VStack {
+            Text("Hello, World #3!")
+            Button(action: {
+                rootIsActive = false
+                print("root is active?: \(rootIsActive)")
+            }, label: {
+                Text("Pop to root")
+            })
+        }
+        .navigationBarTitle("Three")
+        .onAppear {
+            print("root is active?: \(rootIsActive)")
+        }
+
     }
 }
 
