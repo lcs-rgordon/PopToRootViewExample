5366ef17776da7b6ce937a981e45f22049b06946
Russell Gordon <rgordon@lcs.on.ca>
2021-10-18 17:45:16 -0400

    Initial Commit

---
+++ PopToRootViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopToRootViewExample
+//
+//  Created by Russell Gordon on 2021-10-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PopToRootViewExample/PopToRootViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopToRootViewExampleApp.swift
+//  PopToRootViewExample
+//
+//  Created by Russell Gordon on 2021-10-18.
+//
+
+import SwiftUI
+
+@main
+struct PopToRootViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
